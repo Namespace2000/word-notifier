880121991d1f2498f363fbe6978f6a2126ede109
Naveen Kumar <56289356+newday2000@users.noreply.github.com>
2021-01-28 15:51:27 +0530

    word notifier source code

---
+++ app.py
@@ -0,0 +1,60 @@
+import os
+import json
+from datetime import date
+import requests
+from dotenv import load_dotenv
+from twilio.rest import Client
+load_dotenv()
+
+"""TWILIO_ACCOUNT_SID = os.getenv("AC0fe519f5231fb9f55c0ade0cb00e6d21")
+TWILIO_AUTH_TOKEN = os.getenv("45d4da8774711e5c0cfad6c7f071eaf0")
+TWILIO_PHONE_NUMBER = os.getenv("+17864603899")
+RECEIVER_PHONE_NUMBER = os.getenv("+918178145426")
+WORDNIK_API_KEY = os.getenv("WORDNIK_API_KEY")"""
+
+TWILIO_ACCOUNT_SID = "AC0fe519f5231fb9f55c0ade0cb00e6d21"
+TWILIO_AUTH_TOKEN = "45d4da8774711e5c0cfad6c7f071eaf0"
+TWILIO_PHONE_NUMBER = "+17864603899"
+RECEIVER_PHONE_NUMBER = "+918178145426"
+WORDNIK_API_KEY = "WORDNIK_API_KEY"
+
+client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+
+
+def get_word_of_the_day(current_date):
+    """
+    Fetch word of the day from Wordnik API
+    """
+    response_data = {"word": "Sorry, No new word today", "definition": "No definition available"}
+    if WORDNIK_API_KEY:
+        url = f"https://api.wordnik.com/v4/words.json/wordOfTheDay?date={current_date}" \
+              f"&api_key={WORDNIK_API_KEY}"
+        response = requests.get(url)
+        api_response = json.loads(response.text)
+        if response.status_code == 200:
+            response_data["word"] = api_response["word"]
+            for definition in api_response["definitions"]:
+                response_data["definition"] = definition["text"]
+                break
+    else:
+        response_data["word"] = "mesmerizing"
+        response_data["definition"] = "capturing one's attention as if by magic"
+    return response_data
+
+
+def send_sms(response_data):
+    """
+    Send SMS notification with New word
+    """
+    body = str(response_data["word"]) + "\n\n" + str(response_data["definition"])
+    message = client.messages.create(
+        body=body,
+        from_=TWILIO_PHONE_NUMBER,
+        to=RECEIVER_PHONE_NUMBER
+    )
+
+
+if __name__ == "__main__":
+    current_date = date.today()
+    data = get_word_of_the_day(current_date)
+    send_sms(data)
